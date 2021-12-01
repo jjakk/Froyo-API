fc4e718945fbc2d236ecbdd4a76d29ca87ef4a86
John Khachian <jaksawesome10@gmail.com>
2021-11-30 23:04:41 -0500

    Auth Controller

--- Postgres-API/routers/api/auth.js
+++ Postgres-API/routers/api/auth.js
@@ -1,47 +1,8 @@
-const argon2 = require('argon2');
-const jwt = require('jsonwebtoken');
 const { Router } = require('express');
-const pool = require('../../db');
-const queries = require('../../queries/queries');
+const AuthController = require('../../controllers/AuthController');
 
 const router = Router();
 
-router.post('/login', async (req, res) => {
-    try {
-        const { email, passwordAttempt } = req.body;
-
-        // Confirm that email and password aren't empty
-        switch (''){
-            case email:
-                return res.status(400).send('Must provide email');
-            case passwordAttempt:
-                return res.status(400).send('Must provide password');
-        }
-
-        // Query the database for the user
-        const { rows: [ user ] } = await pool.query(queries.users.get('email'), [email]);
-        if(!user) return res.status(400).send('Email not found');
-
-        // Verify the attempted password
-        const validPassword = await argon2.verify(user.password, passwordAttempt);
-        if(!validPassword) return res.status(400).send('Invalid password');
-
-        // Generate JWT token and attach to response header
-        const token = jwt.sign({ userId: user.id }, process.env.TOKEN_KEY);
-
-        // Remove password and other irrelevant information before sending back to client
-        const {
-            password,
-            email_verified,
-            timestamp,
-            ...rest
-        } = user;
-
-        return res.status(200).set('authorization', `Bearer ${token}`).send(rest);
-    }
-    catch (err){
-        return res.status(400).send(err.message);
-    }
-});
+router.post('/login', AuthController.login);
 
 module.exports = router;
