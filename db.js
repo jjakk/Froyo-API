f9af45c1045a44441288472c97a895a108b14514
John Khachian <jaksawesome10@gmail.com>
2022-01-10 16:37:48 -0500

    another method

--- db.js
+++ db.js
@@ -3,8 +3,11 @@ const { Pool } = require('pg');
 const isProduction = process.env.NODE_ENV === 'production';
 const connectionString = `postgresql://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`;
 const pool = new Pool({
-    connectionString: connectionString,
-    ssl: isProduction ? true : false,
+    user: process.env.RDS_USERNAME,
+    host: process.env.RDS_HOSTNAME,
+    database: process.env.RDS_DB_NAME,
+    password: process.env.RDS_PASSWORD,
+    port: process.env.RDS_PORT,
 });
 
 module.exports = pool;
