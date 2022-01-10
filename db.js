0d275deb09cf3281c897f3b5bea03f089e1a50db
John Khachian <jaksawesome10@gmail.com>
2022-01-10 16:29:04 -0500

    correction

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 const connectionString = `postgresql://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`;
 const pool = new Pool({
     connectionString: connectionString,
-    ssl: true,
+    ssl: isProduction ? true : false,
 });
 
 module.exports = pool;
