454781ae54c9ebf236b47f7fd976429da2a27ca6
John Khachian <jaksawesome10@gmail.com>
2022-01-10 16:17:10 -0500

    possible database fix

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const isProduction = process.env.NODE_ENV === 'production';
 const connectionString = `postgresql://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`;
 const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    connectionString: connectionString,
     ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
