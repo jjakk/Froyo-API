e240b935aa9b65a9abcdff0a80379574cf89629d
John Khachian <jaksawesome10@gmail.com>
2022-06-30 23:04:25 -0400

    more progress with takeout

--- controllers/RootController/Takeout/Takeout.js
+++ controllers/RootController/Takeout/Takeout.js
@@ -1,16 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 const queryDB = require('../../../queries/queryDB');
+const { getTakeoutDirectory } = require('@froyo-api/constants');
 
 class Takeout{
     constructor(userId){
         this.userId = userId;
     }
 
+    
+    downloadImages(){}
+    zip(){}
+
+    async downloadProfilePicture(){
+        /*const [{
+            profile_picture_bucket_key
+        }] = await queryDB('users', { where: ['id'] }, [this.userId]);*/
+
+    }
+
     async export(columns){
         for(const column of columns){
             let identification;
             let cells;
+
             switch(column){
                 case 'users':
                     identification = 'id';
@@ -21,6 +34,9 @@ class Takeout{
                 case 'comments':
                     identification = 'author_id';
                     break;
+                case 'messages':
+                    identification = 'author_id';
+                    break;
                 case 'chat_membership':
                     identification = 'user_id';
                     break;
@@ -28,29 +44,64 @@ class Takeout{
                     cells = await queryDB(column, 'get', { where: ['user_a_id', 'user_b_id'], whereCondition: 'OR' }, [this.userId, this.userId]);
                     break;
                 case 'images':
-                    cells = [];
-                    const posts = await queryDB('posts', 'get', { where: ['author_id'] }, [this.userId]);
-                    for(const post of posts){
-                        cells = [
-                            ...cells,
-                            ...await queryDB('images', 'get', { where: ['post_id'] }, [post.id])
-                        ];
-                    }
+                    cells = await this.getImageCells();
                     break;
                 case 'chats':
-                    cells = [];
-                    const chatIds = (await (await queryDB('chat_membership', 'get', { where: ['user_id'] }, [this.userId]))).map(chat => chat.chat_id);
-                    for(const chatId of chatIds){
-                        cells = [
-                            ...cells,
-                            ...await queryDB('chats', 'get', { where: ['chat_id'] }, [chatId])
-                        ];
-                    }
+                    cells = await this.getChatCells();
+                    break;
+                case 'likeness':
+                    cells = await this.getLikenessCells();
                     break;
             }
             if(identification) cells = await queryDB(column, 'get', { where: [identification] }, [this.userId]);
             this.writeCSV(column, cells);
         }
+    }
+
+    async getLikenessCells(){
+        let cells = await queryDB('likeness', 'get', { where: ['user_id'] }, [this.userId]);
+
+        const likenessPosts = await queryDB('posts', 'get', { where: ['author_id'] }, [this.userId]);
+        for(const post of likenessPosts){
+            cells = [
+                ...cells,
+                ...await queryDB('likeness', 'get', { where: ['content_id'] }, [post.id])
+            ];
+        };
+
+
+        const likenessComments = await queryDB('posts', 'get', { where: ['author_id'] }, [this.userId]);
+        for(const comment of likenessComments){
+            cells = [
+                ...cells,
+                ...await queryDB('likeness', 'get', { where: ['content_id'] }, [comment.id])
+            ];
+        }
+        return cells;
+    }
+
+    async getChatCells(){
+        let cells = [];
+        const chatIds = (await (await queryDB('chat_membership', 'get', { where: ['user_id'] }, [this.userId]))).map(chat => chat.chat_id);
+        for(const chatId of chatIds){
+            cells = [
+                ...cells,
+                ...await queryDB('chats', 'get', { where: ['chat_id'] }, [chatId])
+            ];
+        }
+        return cells;
+    }
+
+    async getImageCells(){
+        let cells = [];
+        const imagePosts = await queryDB('posts', 'get', { where: ['author_id'] }, [this.userId]);
+        for(const post of imagePosts){
+            cells = [
+                ...cells,
+                ...await queryDB('images', 'get', { where: ['post_id'] }, [post.id])
+            ];
+        }
+        return cells;
     }
 
     writeCSV(name, cells){
