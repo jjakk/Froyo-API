da664529554b15392d939ed833cff44ae7b61759
John Khachian <jaksawesome10@gmail.com>
2022-07-01 20:01:30 -0400

    Revert to 56e05fced

--- constants/constants.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const CONSTANTS = {
-    getTakeoutDirectory: () => path.resolve(path.join(__dirname, '../takeouts'))
-};
-
-module.exports = CONSTANTS;
--- routers/api/root.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { getRoot, getTakeout } = require('../../controllers/RootController/RootController');
-
-const router = Router();
-
-router.get('/', getRoot);
-router.get('/takeout', getTakeout);
-
-module.exports = router;
