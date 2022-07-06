7f605bb60fcc2596b5245831501349009b14127d
John Khachian <jaksawesome10@gmail.com>
2022-07-05 23:40:35 -0400

    post images

--- constants/constants.js
+++ constants/constants.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const CONSTANTS = {
+    API_ENDPOINT: 'https://api.froyo.social',
     getTakeoutDirectory: () => {
         const takeoutDirectory = path.resolve(path.join(__dirname, '../takeouts'));
         if (!fs.existsSync(takeoutDirectory)) fs.mkdirSync(takeoutDirectory);
