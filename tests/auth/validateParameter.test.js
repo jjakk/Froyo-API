2662e7b95978011009971965169400d630876bec
John Khachian <jaksawesome10@gmail.com>
2022-07-23 23:56:03 -0400

    parameter validation

--- tests/auth/validateParameter.test.js
+++ tests/auth/validateParameter.test.js
@@ -1,3 +1,33 @@
-test('Todo', () => {
-    expect(true).toBe(true);
+const request = require('supertest');
+const app = require('../../app');
+
+test('invalid formatted username', async () => {
+    const response = await request(app).get('/auth/validateParameter/username/sdf-df').send({});
+    expect(response.statusCode).toBe(400);
 });
+
+test('username already in use', async () => {
+    const response = await request(app).get('/auth/validateParameter/username/jester').send({});
+    expect(response.statusCode).toBe(400);
+});
+
+test('valid username', async () => {
+    const response = await request(app).get('/auth/validateParameter/username/asdjfaldfjkdlsd').send({});
+    expect(response.statusCode).toBe(200);
+});
+
+test('invalid formatted email', async () => {
+    const response = await request(app).get('/auth/validateParameter/email/sdfsdf').send({});
+    expect(response.statusCode).toBe(400);
+});
+
+test('email already in use', async () => {
+    const response = await request(app).get('/auth/validateParameter/email/jester@jak.bz').send({});
+    expect(response.statusCode).toBe(400);
+});
+
+test('valid username', async () => {
+    const response = await request(app).get('/auth/validateParameter/email/woejfoiwjfoi@gmail.com').send({});
+    expect(response.statusCode).toBe(200);
+});
+
