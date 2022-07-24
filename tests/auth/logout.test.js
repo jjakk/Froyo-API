20be4732e9a83bd84aeec2c438dda48ca084e284
John Khachian <jaksawesome10@gmail.com>
2022-07-23 23:48:01 -0400

    logout test

--- tests/auth/logout.test.js
+++ tests/auth/logout.test.js
@@ -1,3 +1,7 @@
-test('Todo', () => {
-    expect(true).toBe(true);
+const request = require('supertest');
+const app = require('../../app');
+
+test('logging out', async () => {
+    const response = await request(app).post("/auth/logout").send({});
+    expect(response.statusCode).toBe(200);
 });
