e1ac71c6f92fbf65147c2d06d76e8867273ac20e
John Khachian <jaksawesome10@gmail.com>
2022-07-24 15:27:04 -0400

    test constants, and some user creation tests

--- tests/auth/login.test.js
+++ tests/auth/login.test.js
@@ -1,16 +1,17 @@
 const request = require('supertest');
+const { testUser } = require('../testConstants');
 const app = require('../../app');
 
 test('Missing email', async () => {
     const response = await request(app).post("/auth/login").send({
-        password: 'password'
+        password: testUser.password
     });
     expect(response.statusCode).toBe(422);
 });
 
 test('Missing password', async () => {
     const response = await request(app).post("/auth/login").send({
-        email: 'sdfdjsofijsdiofjosidjfoisjfosdjoifjsdoifjoisdjf@gmail.com'
+        email: testUser.email
     });
     expect(response.statusCode).toBe(422);
 });
@@ -18,7 +19,7 @@ test('Missing password', async () => {
 test('Email & password given. Email not formatted properly', async () => {
     const response = await request(app).post("/auth/login").send({
         email: 'sdfdjsofijsdiofjosidjfoisjfosdjoifjs',
-        password: 'password'
+        password: testUser.password
     });
     expect(response.statusCode).toBe(422);
 });
@@ -26,14 +27,14 @@ test('Email & password given. Email not formatted properly', async () => {
 test('Email & password given. Email not in the database', async () => {
     const response = await request(app).post("/auth/login").send({
         email: 'sdfdjsofijsdiofjosidjfoisjfosdjoifjsdoifjoisdjf@gmail.com',
-        password: 'password'
+        password: testUser.password
     });
     expect(response.statusCode).toBe(422);
 });
 
 test('Email & password given. Password is incorrect', async () => {
     const response = await request(app).post("/auth/login").send({
-        email: 'jester@jak.bz',
+        email: testUser.email,
         password: 'sdjflsdkf'
     });
     expect(response.statusCode).toBe(422);
@@ -41,8 +42,8 @@ test('Email & password given. Password is incorrect', async () => {
 
 test('valid login', async () => {
     const response = await request(app).post("/auth/login").send({
-        email: 'jester@jak.bz',
-        password: 'password'
+        email: testUser.email,
+        password: testUser.password
     });
     expect(response.statusCode).toBe(200);
 });
--- tests/users/createUser.test.js
+++ tests/users/createUser.test.js
@@ -1,3 +1,114 @@
-test('Todo', () => {
-    expect(true).toBe(true);
+const request = require('supertest');
+const app = require('../../app');
+
+test('Missing email', async () => {
+    const response = await request(app).post("/users").send({
+        username: 'createUserTest',
+        dob: new Date('January 1, 2000'),
+        first_name: 'creatUser',
+        last_name: 'test',
+        password: 'password'
+    });
+    expect(response.text).toBe('Must provide an email');
+    expect(response.statusCode).toBe(422);
+});
+
+test('Missing username', async () => {
+    const response = await request(app).post("/users").send({
+        email: 'createUserTest@jak.bz',
+        dob: new Date('January 1, 2000'),
+        first_name: 'creatUser',
+        last_name: 'test',
+        password: 'password'
+    });
+    expect(response.text).toBe('Must provide a username');
+    expect(response.statusCode).toBe(422);
+});
+
+test('Missing dob', async () => {
+    const response = await request(app).post("/users").send({
+        email: 'createUserTest@jak.bz',
+        username: 'createUserTest',
+        first_name: 'creatUser',
+        last_name: 'test',
+        password: 'password'
+    });
+    expect(response.text).toBe('Must provide a date of birth');
+    expect(response.statusCode).toBe(422);
+});
+
+test('Missing first_name', async () => {
+    const response = await request(app).post("/users").send({
+        email: 'createUserTest@jak.bz',
+        username: 'createUserTest',
+        dob: new Date('January 1, 2000'),
+        last_name: 'test',
+        password: 'password'
+    });
+    expect(response.text).toBe('Must provide a first name');
+    expect(response.statusCode).toBe(422);
+});
+
+
+test('Missing last_name', async () => {
+    const response = await request(app).post("/users").send({
+        email: 'createUserTest@jak.bz',
+        username: 'createUserTest',
+        dob: new Date('January 1, 2000'),
+        first_name: 'creatUser',
+        password: 'password'
+    });
+    expect(response.text).toBe('Must provide a last name');
+    expect(response.statusCode).toBe(422);
+});
+
+test('Missing password', async () => {
+    const response = await request(app).post("/users").send({
+        email: 'createUserTest@jak.bz',
+        username: 'createUserTest',
+        dob: new Date('January 1, 2000'),
+        first_name: 'creatUser',
+        last_name: 'test'
+    });
+    expect(response.text).toBe('Must provide a password');
+    expect(response.statusCode).toBe(422);
+});
+
+test('All fields are given. Email formatted incorrectly', async () => {
+    const response = await request(app).post("/users").send({
+        email: 'createUserTest',
+        username: 'createUserTest',
+        dob: new Date('January 1, 2000'),
+        first_name: 'creatUser',
+        last_name: 'test',
+        password: 'password'
+    });
+    expect(response.text).toBe('Not a valid email');
+    expect(response.statusCode).toBe(422);
+});
+
+test('All fields are given. Username formatted incorrectly', async () => {
+    const response = await request(app).post("/users").send({
+        email: 'createUserTest@jak.bz',
+        username: 'createUserTest-1',
+        dob: new Date('January 1, 2000'),
+        first_name: 'creatUser',
+        last_name: 'test',
+        password: 'password'
+    });
+    expect(response.text).toBe('Not a valid username');
+    expect(response.statusCode).toBe(422);
+});
+
+test('All fields are given. Age is <13 years old', async () => {
+    const response = await request(app).post("/users").send({
+        email: 'createUserTest@jak.bz',
+        username: 'createUserTest',
+        dob: new Date(),
+        first_name: 'creatUser',
+        last_name: 'test',
+        password: 'password'
+    });
+    expect(response.text).toBe('Must be at least 13 years old to create an account');
+    expect(response.statusCode).toBe(422);
 });
