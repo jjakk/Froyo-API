917a46449389e022a957788f9ad0343107690f84
John Khachian <jaksawesome10@gmail.com>
2022-07-25 19:24:01 -0400

    not signed in test

--- tests/users/getUserById.test.js
+++ tests/users/getUserById.test.js
@@ -1,3 +1,9 @@
-test('Todo', () => {
-    expect(true).toBe(true);
+const request = require('supertest');
+const { testUser } = require('../testConstants');
+const app = require('../../app');
+
+test('Not signed in', async () => {
+    const response = await request(app).get('/users/123');
+    expect(response.text).toBe("You're not logged in");
+    expect(response.statusCode).toBe(200);
 });
