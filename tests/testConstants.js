d404162311ed70318b6230bf8e136f936c09368c
John Khachian <jaksawesome10@gmail.com>
2022-07-30 16:18:52 -0400

    get user by id tests

--- tests/testConstants.js
+++ tests/testConstants.js
@@ -1,13 +1,28 @@
+const request = require('supertest');
+const app = require('../app');
+
+const testUser = {
+    email: "jester@jak.bz",
+    username: "jester",
+    dob: new Date("November 23, 1995"),
+    first_name: "jester",
+    last_name: "lester",
+    password: "password"
+}
+
+const getAuthToken = async () => {
+    const { headers: { authorization } } = await request(app)
+        .post('/auth/login')
+        .send({
+            email: testUser.email,
+            password: testUser.password
+        });
+    return authorization;
+}
 
 const TEST_CONSTANTS = {
-    testUser: {
-        email: "jester@jak.bz",
-        username: "jester",
-        dob: new Date("November 23, 1995"),
-        first_name: "jester",
-        last_name: "lester",
-        password: "password"
-    }
+    testUser,
+    getAuthToken
 };
 
 module.exports = TEST_CONSTANTS;
--- tests/users/getUserById.test.js
+++ tests/users/getUserById.test.js
@@ -1,9 +1,23 @@
 const request = require('supertest');
-const { testUser } = require('../testConstants');
+const { getAuthToken } = require('../testConstants');
 const app = require('../../app');
 
 test('Not signed in', async () => {
     const response = await request(app).get('/users/123');
     expect(response.text).toBe("You're not logged in");
     expect(response.statusCode).toBe(200);
+});
+
+test('Invalid user ID format', async () => {
+    const attmeptedId = '123';
+    const response = await request(app).get(`/users/${attmeptedId}`).set('Authorization', await getAuthToken());
+    expect(response.text).toBe(`invalid input syntax for type uuid: "${attmeptedId}"`);
+    expect(response.statusCode).toBe(400);
+});
+
+test("User doesn't exist", async () => {
+    const attemptedId = '00000000-0000-0000-0000-000000000000';
+    const response = await request(app).get(`/users/${attemptedId}`).set('Authorization', await getAuthToken());
+    expect(response.text).toBe('User not found');
+    expect(response.statusCode).toBe(404);
 });
