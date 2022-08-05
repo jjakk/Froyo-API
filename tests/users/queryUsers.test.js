e723b61a81987ee6a9951476d47641eb32c0a358
John Khachian <jaksawesome10@gmail.com>
2022-08-05 13:46:31 -0400

    query users not signed in test case

--- tests/users/queryUsers.test.js
+++ tests/users/queryUsers.test.js
@@ -1,3 +1,8 @@
-test('Todo', () => {
-    expect(true).toBe(true);
+const request = require('supertest');
+const app = require('../../app');
+
+test('Not signed in', async () => {
+    const response = await request(app).get('/users');
+    expect(response.text).toBe('You\'re not logged in');
+    expect(response.statusCode).toBe(200);
 });
